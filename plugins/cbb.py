525bedd1721d5fb19456b28f9b4e5d2acd9d3d92
sainipkjgithub <160867713+sainipkjgithub@users.noreply.github.com>
2024-12-18 09:26:54 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>Mr. Singodiya</a>\n○ Channel : https://t.me/aibots72\n○ Support Group : https://t.me/aibotes</b>",
+            text = f"<b>○ <a href='tg://user?id=6150091802'>Owner</a> : <a href='http://t.me/mr_singodiyabot'>✪Mr. Singodiya✪</a>\n○ Channel : https://t.me/aibots72\n○ Support Group : https://t.me/aibotes</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
